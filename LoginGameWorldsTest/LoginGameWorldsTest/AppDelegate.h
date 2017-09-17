bd9bfb900c26933ab7ae51f06102521e6234ac9d
Andrew <shevtsov.ap@gmail.com>
2017-09-17 12:06:39 +0300

    create project

---
+++ LoginGameWorldsTest/LoginGameWorldsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginGameWorldsTest
+//
+//  Created by ASH on 9/17/17.
+//  Copyright © 2017 SAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginGameWorldsTest/LoginGameWorldsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoginGameWorldsTest
+//
+//  Created by ASH on 9/17/17.
+//  Copyright © 2017 SAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
